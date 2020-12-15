fe9c098d9da985c8ded2cd9d76cbfffa8c9f8027
es2k <edwsun@umich.edu>
2020-12-14 19:54:21 -0500

    idk

--- models/logger.py
+++ models/logger.py
@@ -15,7 +15,11 @@ class Logger(object):
             self.writer.flush()
 
     def image_summary(self, tag, image, step):
-        s = BytesIO()
+        with self.writer.as_default():
+            tf.summary.image(tag, image,step=step)
+            self.writer.flush()
+
+        '''s = BytesIO()
         im = Image.fromarray(image)
         im.save(s+".png")
 
@@ -29,30 +33,13 @@ class Logger(object):
         # Create and write Summary
         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, image=img_sum)])
         self.writer.add_summary(summary, step)
-        self.writer.flush()
+        self.writer.flush()'''
 
     def image_list_summary(self, tag, images, step):
         if len(images) == 0:
             return
         img_summaries = []
         for i, img in enumerate(images):
-            s = BytesIO()
-            im = Image.fromarray(img)
-            im.save(s+".png")
-
-            # Create an Image object
-            img_sum = tf.Summary.Image(
-                encoded_image_string=s.getvalue(),
-                height=img.shape[0],
-                width=img.shape[1],
-            )
-
-            # Create a Summary value
-            img_summaries.append(
-                tf.Summary.Value(tag="{}/{}".format(tag, i), image=img_sum)
-            )
-
-        # Create and write Summary
-        summary = tf.Summary(value=img_summaries)
-        self.writer.add_summary(summary, step)
-        self.writer.flush()
+            with self.writer.as_default():
+                tf.summary.image(tag+'/'+str(i), img, step=step)
+                self.writer.flush()
