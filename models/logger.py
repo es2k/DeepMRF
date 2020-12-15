49bfc58a140f227de5d48bec55175d0d4a1c89ef
es2k <edwsun@umich.edu>
2020-12-14 19:07:56 -0500

    Merge remote-tracking branch 'origin/master'

--- models/logger.py
+++ models/logger.py
@@ -13,9 +13,6 @@ class Logger(object):
         with self.writer.as_default():
             tf.summary.scalar(tag, value,step=step)
             self.writer.flush()
-        '''summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
-        self.writer.flush()'''
 
     def image_summary(self, tag, image, step):
         s = BytesIO()
